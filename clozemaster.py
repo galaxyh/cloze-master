9cc8240ddee5f5b56bc0f7c76152b7571db30055
Yu-chun Huang <code@yullage.com>
2015-12-24 17:08:39 +0800

    Fix numpy ndarray cannot send with MessagePack problem.

--- clozemaster.py
+++ clozemaster.py
@@ -1,6 +1,7 @@
 from mprpc import RPCClient
 
 client = RPCClient('127.0.0.1', 1979)
-print client.call('doesnt_match', 'apple boat banana orange')
+print client.call('doesnt_match', ['apple', 'boat', 'banana'])
+print client.call('similarity', 'man', 'woman')
+print client.call('most_similar', ['woman', 'king'], ['male'], 10, None)
 print client.call('vector', 'fish')
-print client.call('most_similar', positive=['woman', 'king'], negative=['man'])
