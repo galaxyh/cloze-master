1f4750e6b16820aa3bfdefea1caced45ca8ca0eb
Yu-chun Huang <code@yullage.com>
2016-01-05 19:36:28 +0800

    Bug fix.

--- analyze_avgemb.py
+++ analyze_avgemb.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
         random_ev = 0
         records = csv.reader(f, delimiter=',')
         for record in records:
-            avgemb_ev += (1.0 / record[1]) if record[1] > 0 else 0
-            random_ev += (1.0 / record[2]) if record[2] > 0 else 0
+            avgemb_ev += (1.0 / int(record[1])) if int(record[1]) > 0 else 0
+            random_ev += (1.0 / int(record[2])) if int(record[2]) > 0 else 0
 
         print 'Expected value of correct answer'
         print 'Average embedding = {.3f}'.format(avgemb_ev)
