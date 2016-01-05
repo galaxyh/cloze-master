aceb88b9d311c1a489cf787b574fbb57ce9ed6d3
Yu-chun Huang <code@yullage.com>
2016-01-05 19:44:38 +0800

    Bug fix.

--- analyze_avgemb.py
+++ analyze_avgemb.py
@@ -16,17 +16,19 @@ if __name__ == '__main__':
         quit()
 
     with codecs.open(options.filename.strip(), 'r', encoding='utf8') as f:
+        question_count = 0
         avgemb_ev = 0
         random_ev = 0
         records = csv.reader(f, delimiter=',')
         for record in records:
+            question_count += 1
             avgemb_ev += (1.0 / int(record[1])) if int(record[1]) > 0 else 0
             random_ev += (1.0 / int(record[2])) if int(record[2]) > 0 else 0
 
-        print 'Total questions answered =', len(records)
+        print 'Total questions answered =', question_count
         print 'Expected value of correct answer:'
         print 'Average embedding = {:<.3f}'.format(avgemb_ev)
         print 'Random select     = {:<.3f}'.format(random_ev)
         print 'Expected correct answer rate:'
-        print 'Average embedding = {:<.3f}'.format(avgemb_ev/len(records))
-        print 'Random select     = {:<.3f}'.format(random_ev/len(records))
+        print 'Average embedding = {:<.3f}'.format(avgemb_ev/question_count)
+        print 'Random select     = {:<.3f}'.format(random_ev/question_count)
