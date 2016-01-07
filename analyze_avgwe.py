e39a0e2095ace6a6a405bf5c0bb3c1d6fcee736a
Yu-chun Huang <code@yullage.com>
2016-01-08 06:55:44 +0800

    Add average entities information.

--- analyze_avgwe.py
+++ analyze_avgwe.py
@@ -18,18 +18,24 @@ if __name__ == '__main__':
     with codecs.open(options.filename.strip(), 'r', encoding='utf8') as f:
         num_question = 0
         correct_candidate = 0
+        num_candidate_entity = 0
+        num_all_entity = 0
         avgwe_ev = 0
         random_ev = 0
         records = csv.reader(f, delimiter=',')
         for record in records:
             num_question += 1
-            correct_candidate += 1 if int(record[0]) == 1 else 0
+            correct_candidate += int(record[0])
+            num_candidate_entity += int(record[1])
+            num_all_entity += record[2]
             avgwe_ev += (1.0 / int(record[1])) if int(record[1]) > 0 else 0
             random_ev += (1.0 / int(record[2])) if int(record[2]) > 0 else 0
 
         print 'Total questions answered =', num_question
         print 'Correct candidate sentences =', correct_candidate
-        print 'Correct candidate sentences rate = {:>1.3f}'.format(float(correct_candidate) / num_question)
+        print 'Correct candidate sentences rate = {:>1.3f}'.format(correct_candidate / num_question)
+        print 'Average number of entities in a question = {:>1.3f}'.format(num_all_entity / num_question)
+        print 'Average number of entities in a candidate sentence = {:>1.3f}'.format(num_candidate_entity / num_question)
         print
         print 'Expected value of correct answering:'
         print 'Average WE = {:>4.3f}'.format(avgwe_ev)
